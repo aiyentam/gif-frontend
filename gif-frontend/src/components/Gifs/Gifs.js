bd8961871c4acbf813cbf2308c4581a4311f7d4f
Ai Yen Tam <tam.aiyen@gmail.com>
2018-03-16 16:44:37 -0400

    components & styling

--- gif-frontend/src/components/Gifs/Gifs.js
+++ gif-frontend/src/components/Gifs/Gifs.js
@@ -1,47 +1,38 @@
 import React, { Component } from "react";
+import "./Gifs.css";
 
 class Gifs extends Component {
   render() {
     return (
-      <div>
-        <div class="gifs">
-          <div id="modal-create" class="modal">
-            <div class="modal-content">
+      <div className="all">
+        <div>
+          <div>
+            <div>
               <h4>Create a New Gif!</h4>
-              <div class="row">
-                <div class="input-field">
-                  <input id="name" type="text" class="validate" />
-                  <label for="name">Name</label>
-                </div>
-                <div class="input-field">
-                  <input id="url" type="text" class="validate" />
-                  <label for="url">URL</label>
-                </div>
-                <button class="btn grey center-align" id="submit">
-                  Submit
-                </button>
+              <div>
+                <form onSubmit={this.props.handleSubmit}>
+                  <label>Name</label>
+                  <input type="text" onChange={this.props.handleChangeName} />
+                  <label>Url</label>
+                  <input type="text" onChange={this.props.handleChangeUrl} />
+                  <button type="submit">Submit</button>
+                </form>
               </div>
             </div>
           </div>
         </div>
-        <div id="modal-edit" class="modal">
-          <div class="modal-content">
-            <h4>Create a New Gif!</h4>
-            <div class="row">
-              <div class="input-field">
-                <input id="name-edit" type="text" class="validate" />
-                <label for="name-edit">Name</label>
-              </div>
-              <div class="input-field">
-                <input id="url-edit" type="text" class="validate" />
-                <label for="url-edit">URL</label>
-              </div>
-              <button class="btn grey center-align" id="submit-edit">
-                Submit
-              </button>
-              <button class="btn red center-align" id="delete">
-                Delete
-              </button>
+        <div>
+          <div>
+            <h4>Edit a New Gif!</h4>
+            <div>
+              <form onSubmit={this.props.handleSubmit}>
+                <label>Name</label>
+                <input type="text" onChange={this.props.handleChangeName} />
+                <label>Url</label>
+                <input type="text" onChange={this.props.handleChangeUrl} />
+                <button type="submit">Submit</button>
+              </form>
+              <button type="submit">Delete</button>
             </div>
           </div>
         </div>
--- gif-frontend/src/components/Nav/Nav.js
+++ gif-frontend/src/components/Nav/Nav.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Gifs from "../Gifs/Gifs";
+import "./Nav.css";
+import axios from "axios";
 
 class Nav extends Component {
   constructor(props) {
@@ -12,17 +14,61 @@ class Nav extends Component {
         }
       ]
     };
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeUrl = this.handleChangeUrl.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChangeName(event) {
+    this.setState({ name: event.target.value.toLowerCase() });
+  }
+
+  handleChangeUrl(event) {
+    this.setState({ imgUrl: event.target.value.toLowerCase() });
+  }
+
+  handleSubmit(event) {
+    alert("You submitted a Gif!");
+    event.preventDefault();
+    const uploadedImg = {
+      name: this.state.gifs.name,
+      imgUrl: this.state.gifs.imgUrl
+    };
+
+    axios.post("http://localhost:3000/gifs", { uploadedImg }).then(res => {
+      console.log(res);
+      console.log(res.data);
+    });
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3000/")
+      .then(res => {
+        const gifs = res.data;
+        this.setState({ gifs });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div>
         <nav>
-          <div>
-            <a href="https://giphy.com/">GAphy</a>
-            <button>+</button>
+          <div className="nav-wrapper">
+            <a href="https://giphy.com/" className="logo">
+              GAphy
+            </a>
+            <button className="btn">+</button>
           </div>
         </nav>
-        <Gifs />
+        <Gifs
+          handleChangeUrl={this.handleChangeUrl}
+          handleChangeName={this.handleChangeName}
+          handleSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
