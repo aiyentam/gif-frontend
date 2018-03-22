cf83894ce6eeabb771ed9f2d5d5016d374ad0c63
Ai Yen Tam <tam.aiyen@gmail.com>
2018-03-22 00:07:56 -0400

    style & buttons

--- gif-frontend/src/components/App/App.js
+++ gif-frontend/src/components/App/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Redirect,
-  Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 import axios from "axios";
 import "./App.css";
@@ -16,42 +10,60 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gifs: [
-        {
-          name: "",
-          imgUrl: ""
-        }
-      ],
+      gif: {
+        name: "",
+        imgUrl: ""
+      },
+      gifs: [],
       succeeded: false
     };
     this.handleChangeName = this.handleChangeName.bind(this);
     this.handleChangeUrl = this.handleChangeUrl.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleChangeName(event) {
-    this.setState({ name: event.target.value.toLowerCase() });
+    console.log(event.target.value.toLowerCase());
+    this.setState({
+      gif: { ...this.state.gif, name: event.target.value.toLowerCase() }
+    });
   }
 
   handleChangeUrl(event) {
-    this.setState({ imgUrl: event.target.value.toLowerCase() });
+    this.setState({
+      gif: { ...this.state.gif, imgUrl: event.target.value.toLowerCase() }
+    });
   }
 
   handleSubmit(event) {
-    alert("You submitted a Gif!");
+    // alert("You submitted a Gif!");
     event.preventDefault();
     const uploadedImg = {
-      name: this.state.gifs.name,
-      imgUrl: this.state.gifs.imgUrl
+      name: this.state.gif.name,
+      imgUrl: this.state.gif.imgUrl
     };
 
     axios.post("http://localhost:3001/gifs", { uploadedImg }).then(res => {
-      console.log(res);
       console.log(res.data);
       var newArray = this.state.gifs.slice();
       newArray.push({ name: res.data.name, imgUrl: res.data.url });
-      this.setState(newArray);
-      this.setState({ succeeded: true });
+      this.setState({ gifs: newArray });
+      console.log(this.state.gifs);
+    });
+  }
+
+  handleDelete(event) {
+    const arrayToDeleteFrom = this.state.gifs;
+    console.log("Size Before: " + arrayToDeleteFrom);
+    console.log(event.target.value);
+    const objectId = this.state.gifs[event.target.value]._id;
+    console.log(objectId);
+
+    axios.delete("http://localhost:3001/gifs/" + objectId).then(res => {
+      const gifs = res.data;
+      this.setState({ gifs });
+      console.log(this.state.gifs.length);
     });
   }
 
@@ -78,11 +90,7 @@ class App extends Component {
             <Link to="/create">
               <button>add</button>
             </Link>
-            <Link to="/edit">
-              <button>edit</button>
-            </Link>
           </div>
-
           <Switch>
             <Route
               path="/create"
@@ -111,7 +119,22 @@ class App extends Component {
           </Switch>
           <div className="uploads">
             <h4>Uploaded Gifs</h4>
-            <div>image here</div>
+            <div>
+              {this.state.gifs.map((gif, index) => {
+                return (
+                  <div key={index} className="imgs">
+                    <img src={gif.url} className="gif" />
+                    <span className="gifTitle">{gif.name}</span>
+                    <Link to="/edit">
+                      <button>edit</button>
+                    </Link>
+                    <button value={index} onClick={this.handleDelete}>
+                      delete
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </Router>
